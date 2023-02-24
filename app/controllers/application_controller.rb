953ef77c06f025f639344df18bc104288d1969e4
Rawllings Kiprop <rutojr01@gmail.com>
2023-02-24 16:31:06 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  get '/bakeries' do
+    # "Hello World"
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json
+  end
+
+  get '/baked_goods/by_price' do
+    price = BakedGood.all.order(:price)
+    price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+   expensive = BakedGood.all.order(:price).limit(1)
+   expensive.to_json
+  end
+  
 
 end
